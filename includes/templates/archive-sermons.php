51ada6adbf2547d20b5683f568e58cc9b51f73bb
Matt Ryan <matt.ryan@webdevstudios.com>
2023-08-02 18:03:59 -0400

    Remove debug code

--- includes/templates/archive-sermons.php
+++ includes/templates/archive-sermons.php
@@ -1,10 +1,6 @@
 <?php
 //* Display values of custom fields (those that are not empty)
-// add_action( 'kadence_entry_header', 'display_custom_sermon_fields' );
 display_custom_sermon_fields();
-//* Add post image in Entry Content above Excerpt
-// add_action( 'kadence_single_before_entry_content', 'display_sermon_featured_image', 9 );
 display_sermon_featured_image();
-// add_action( 'kadence_after_content', 'custom_sermon_post_meta' );
 custom_sermon_post_meta();
 
